8643fd10826722103029610083c69704bf6cb630
TamannaRahman2417 <tamannarahman0021@gmail.com>
2022-09-28 22:08:58 +0600

    changes

--- app/Http/Controllers/applicantControl.php
+++ app/Http/Controllers/applicantControl.php
@@ -8,6 +8,21 @@ class applicantControl extends Controller
 {
     //
     public function applicantSetup(Request $request){
-        dd($request->all());
+        $formfields = $request->validate([
+            'name' => 'string| required',
+            'email' => 'email|required',
+            'phone' => 'nullable | string',
+            'gender'=> 'nullable | string',
+            'dob' => 'nullable | string',
+            'latest_degree' => 'nullable | string', 
+            'latest_institute' => 'nullable |string',
+            'present_address' => 'nullable | string',
+            'cover_letter' => 'nullable | string',
+        ]);
+        // if($request->hasFile('cv')){
+        //     $path = 'CV';
+        //     $formfields['cv'] = FileuploadHelpers::fileStore($request->cv, $path);
+        // }
+        dd($formfields);
     }
 }
--- resources/views/applicant/setup.blade.php
+++ resources/views/applicant/setup.blade.php
@@ -9,13 +9,13 @@
             </h2>
             <p class="mb-4">Setup your profile. This informations will be added to email with your CV</p>
         </header>
-        {{dd($profile)}}
+        
         <form method="POST" action="/applicant/profileSetup" enctype="multipart/form-data">
             @csrf
             <div class="mb-6">
                 <label for="name" class="inline-block text-lg mb-2">Name</label>
                 <input type="text" class="border border-gray-200 rounded p-2 w-full" name="name"
-                    value="{{ $profile->name }}" />
+                    value="{{ $profile[0]->name }}" />
                 @error('name')
                     <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
                 @enderror
@@ -24,7 +24,7 @@
             <div class="mb-6">
                 <label for="email" class="inline-block text-lg mb-2">Email</label>
                 <input type="email" class="border border-gray-200 rounded p-2 w-full" name="email"
-                    value="{{ $profile->email }}" />
+                    value="{{ $profile[0]->email }}" />
 
                 @error('email')
                     <p class="text-red-500 text-xs mt-1">
@@ -36,7 +36,7 @@
             <div class="mb-6">
                 <label for="phone" class="inline-block text-lg mb-2">Phone</label>
                 <input type="text" class="border border-gray-200 rounded p-2 w-full" name="phone"
-                    value="{{ $profile->phone }}" />
+                    value="{{ $profile[0]->phone }}" />
 
                 @error('phone')
                     <p class="text-red-500 text-xs mt-1">
@@ -48,10 +48,10 @@
             <div class="mb-6">
                 <label for="gender" class="inline-block text-lg mb-2">Gender</label>
                     <select name="gender" class="border border-gray-200 rounded p-2 w-full">
-                        <option value="" {{ $profile->gender == "" ? 'selected' : '' }}>Select</option>
-                        <option value="male" {{ $profile->gender == "male" ? 'selected' : '' }}>Male</option>
-                        <option value="female" {{ $profile->gender == "female" ? 'selected' : '' }}>Female</option>
-                        <option value="others" {{ $profile->gender == "others" ? 'selected' : '' }}>Others</option>
+                        <option value="" {{ $profile[0]->gender == "" ? 'selected' : '' }}>Select</option>
+                        <option value="male" {{ $profile[0]->gender == "male" ? 'selected' : '' }}>Male</option>
+                        <option value="female" {{ $profile[0]->gender == "female" ? 'selected' : '' }}>Female</option>
+                        <option value="others" {{ $profile[0]->gender == "others" ? 'selected' : '' }}>Others</option>
                     </select>
                 @error('gender')
                     <p class="text-red-500 text-xs mt-1">
@@ -63,7 +63,7 @@
             <div class="mb-6">
                 <label for="dob" class="inline-block text-lg mb-2">Date of Birth</label>
                 <input type="date" class="border border-gray-200 rounded p-2 w-full" name="dob"
-                    value="{{ $profile->dob }}" />
+                    value="{{ $profile[0]->dob }}" />
                 @error('dob')
                     <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
                 @enderror
@@ -72,7 +72,7 @@
             <div class="mb-6">
                 <label for="latest_degree" class="inline-block text-lg mb-2">Latest Degree</label>
                 <input type="text" class="border border-gray-200 rounded p-2 w-full" name="latest_degree"
-                    value="{{ $profile->latest_degree }}" />
+                    value="{{ $profile[0]->latest_degree }}" />
                 @error('latest_degree')
                     <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
                 @enderror
@@ -81,7 +81,7 @@
             <div class="mb-6">
                 <label for="latest_institute" class="inline-block text-lg mb-2">Latest Institute</label>
                 <input type="text" class="border border-gray-200 rounded p-2 w-full" name="latest_institute"
-                    value="{{ $profile->latest_institute }}" />
+                    value="{{ $profile[0]->latest_institute }}" />
                 @error('latest_institute')
                     <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
                 @enderror
@@ -90,7 +90,7 @@
             <div class="mb-6">
                 <label for="present_address" class="inline-block text-lg mb-2">Present Address</label>
                 <input type="text" class="border border-gray-200 rounded p-2 w-full" name="present_address"
-                    value="{{ $profile->present_address }}" />
+                    value="{{ $profile[0]->present_address }}" />
                 @error('present_address')
                     <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
                 @enderror
@@ -101,7 +101,7 @@
                     Cover Letter
                 </label>
                 <textarea class="border border-gray-200 rounded p-2 w-full" name="cover_letter" rows="10"
-                    placeholder="Your cover letter here" value="{{ $profile->cover_letter }}"></textarea>
+                    placeholder="Your cover letter here" value="{{ $profile[0]->cover_letter }}"></textarea>
                 @error('cover_letter')
                     <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
                 @enderror
